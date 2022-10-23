c51286124c17e95a1baa169405b811d18b263f31
Aakash Kulkarni <aakashkulkarni36@gmail.com>
2022-10-23 15:51:09 -0700

    Nothing changed

--- MyLibrary/Tests/MyLibraryTests/File.swift
+++ MyLibrary/Tests/MyLibraryTests/File.swift
@@ -1,28 +1,28 @@
 //
 //  File.swift
 //  
-////
-////  Created by Aakash Kulkarni on 10/15/22.
-////
 //
-//import XCTest
-//import MyLibrary
-//@testable import MyLibrary
+//  Created by Aakash Kulkarni on 10/15/22.
 //
-//final class MyIntegrationTest: XCTestCase {
-//    
-//    func testingWeatherModel() async throws {
-//        
-//        // given
-//        let weatherService = WeatherServiceImpl()
-//        
-//        //when
-//        let temperature = try await weatherService.getTemperature()
-//        
-//        
-//        //then
-//        XCTAssertEqual(temperature, 59)
-//        
-//    }
-//}
+
+import XCTest
+import MyLibrary
+@testable import MyLibrary
+
+final class MyIntegrationTest: XCTestCase {
+    
+    func testingWeatherModel() async throws {
+        
+        // given
+        let weatherService = WeatherServiceImpl()
+        
+        //when
+        let temperature = try await weatherService.getTemperature()
+        
+        
+        //then
+        XCTAssertEqual(temperature, 59)
+        
+    }
+}
     
